139eec099c4ed3572374e921a2a8c0a82f980554
Valeriy Tetevin <seal.matob@gmail.com>
2023-12-10 00:11:40 +0100

    fix question parsing for stage 8

--- app/main.go
+++ app/main.go
@@ -6,13 +6,15 @@ import (
 	"net"
 
 	"github.com/codecrafters-io/dns-server-starter-go/app/types"
-	// Uncomment this block to pass the first stage
-	// "net"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	resolver, err := GetDNSResolver()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resolver.conn.Close()
 
 	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
 	if err != nil {
@@ -49,7 +51,7 @@ func main() {
 
 		fmt.Printf("Parsed Message: %+v, \n \n", in)
 
-		msg := MakerResponse(in)
+		msg := MakerResponse(resolver, in)
 		_, err = udpConn.WriteToUDP(msg.Serialize(), source)
 		if err != nil {
 			fmt.Println("Failed to send response:", err)
@@ -57,7 +59,7 @@ func main() {
 	}
 }
 
-func MakerResponse(in types.DNSMessage) types.DNSMessage {
+func MakerResponse(resolver *dnsResolver, in types.DNSMessage) types.DNSMessage {
 	rcode := types.NOERROR
 
 	if in.Header.OPCODE != types.QUERY {
@@ -68,17 +70,10 @@ func MakerResponse(in types.DNSMessage) types.DNSMessage {
 	answers := []types.DNSAnswer{}
 
 	if rcode == types.NOERROR {
-		for _, q := range questions {
-			if q.QType == types.QTYPE(types.A) {
-				answers = append(answers, types.DNSAnswer{
-					Name:     q.QName,
-					Type:     types.A,
-					Class:    types.IN,
-					TTL:      60,
-					RDLenght: 4,
-					RDATA:    []byte{8, 8, 8, 8},
-				})
-			}
+		var err error
+		answers, err = resolver.ResolveAddress(questions)
+		if err != nil {
+			rcode = types.SERVFAIL
 		}
 	}
 
@@ -101,4 +96,63 @@ func MakerResponse(in types.DNSMessage) types.DNSMessage {
 		Questions: questions,
 		Answers:   answers,
 	}
+}
+
+type dnsResolver struct {
+	conn net.Conn
+}
+
+func GetDNSResolver() (*dnsResolver, error) {
+	addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:53")
+	if err != nil {
+		return nil, fmt.Errorf("Cannot resolve local DNS server address: %v", err)
+	}
+
+	conn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot dial local DNS server: %v", err)
+	}
+
+	return &dnsResolver{
+		conn: conn,
+	}, nil
+}
+
+func (dnsr *dnsResolver) ResolveAddress(quesions types.DBSQuestions) ([]types.DNSAnswer, error) {
+	msg := types.DNSMessage{
+		Header: types.DNSHeader{
+			ID:      1,
+			OPCODE:  types.QUERY,
+			RD:      1,
+			QDCount: uint16(len(quesions)),
+		},
+		Questions: quesions,
+	}
+
+	_, err := dnsr.conn.Write(msg.Serialize())
+	if err != nil {
+		return nil, fmt.Errorf("Cannot send message to local DNS server: %v", err)
+	}
+
+	buf := make([]byte, 512)
+	size, err := dnsr.conn.Read(buf)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot read response from local DNS server: %v", err)
+	}
+
+	res := types.DNSMessage{}
+	err = res.Parse(buf[:size])
+	if err != nil {
+		return nil, fmt.Errorf("Cannot parse response from local DNS server: %v", err)
+	}
+
+	if res.Header.RCODE != types.NOERROR {
+		return nil, fmt.Errorf("Local DNS server returned error: %v", res.Header.RCODE)
+	}
+
+	if len(res.Answers) == 0 {
+		return nil, fmt.Errorf("Local DNS server returned no answers")
+	}
+
+	return res.Answers, nil
 }
